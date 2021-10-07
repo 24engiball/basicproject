43bbda1d4aa421f8b659d34e6ee5d55722135923
24engiball <engiball@gmail.com>
2021-10-07 14:17:34 +0700

    hello

---
+++ runmanrun/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1633589841,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/fruit-sheet0.png",
+		"images/playbutton-sheet0.png",
+		"images/leftbutton-sheet0.png",
+		"images/rightbutton-sheet0.png",
+		"images/upbutton-sheet0.png",
+		"images/replaybutton-sheet0.png",
+		"media/242857__plasterbrain__coin-get.ogg",
+		"media/melodyloops-original-adrenaline-2m30s.m4a",
+		"media/melodyloops-original-adrenaline-2m30s.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
